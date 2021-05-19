6f06d2ea65e181734e7218ef194dfdcdab8c063f
Guto <luiz.v.nasc@gmail.com>
2021-05-19 16:53:54 -0300

    criado script da linha l04 de dourados

---
+++ db/linhas/dourados/l04.js
@@ -0,0 +1,312 @@
+db.dourados.insertOne({
+    "lastUpdate": "19/05/2021",
+    "codigo": "L04",
+    "nome": "Jd. Guaicurus / Diocleio Artuzzi / Esplanada",
+    "observacao": null,
+    "pontos": [
+        {
+            "nome": "Bairro",
+            "sentido": "centro",
+            "horarios": [
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "04:15",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "04:40",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "05:40",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "06:30",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "07:10",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "07:10",
+                    "observacao": "Horário realizado pela linha Distrito Industrial"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "08:20",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "09:35",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "11:25",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "13:10",
+                    "observacao": "Via Esplanada"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "14:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "15:30",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "17:05",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "19:10",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "20:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "21:20",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "04:25",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "05:40",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "07:10",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "08:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "11:00",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "13:00",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "14:10",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "15:00",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "16:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "17:00",
+                    "observacao": "Horário realizado pela linha Distrito Industrial"
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "18:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 2,
+                    "hora_saida": "20:00",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 2,
+                    "hora_saida": "20:00",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                }
+            ],
+            "lat": null,
+            "lon": null,
+            "ordem": 0
+        },
+        {
+            "nome": "Terminal",
+            "sentido": "bairro",
+            "horarios": [
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "05:55",
+                    "observacao": "Via Res. Idelfonso Pedroso"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "06:35",
+                    "observacao": "Via Res. Idelfonso Pedroso"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "07:55",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "09:00",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "10:50",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "12:35",
+                    "observacao": "Via Res. Idelfonso Pedroso / Via Esplanada"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "13:45",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "14:55",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "16:30",
+                    "observacao": "Via Res. Idelfonso Pedroso"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "17:50",
+                    "observacao": "Via Res. Idelfonso Pedroso"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "18:30",
+                    "observacao": "Via Res. Idelfonso Pedroso"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "19:45",
+                    "observacao": "Via Res. Idelfonso Pedroso"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "20:40",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "21:00",
+                    "observacao": "Horário realizado pela linha Distrito Industrial"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "22:30",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 0,
+                    "hora_saida": "23:25",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "06:30",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "07:50",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "10:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "12:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "13:30",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "14:20",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "15:40",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "16:20",
+                    "observacao": "Horário realizado pela linha Distrito Industrial"
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "17:45",
+                    "observacao": null
+                },
+                {
+                    "tipo_dia": 1,
+                    "hora_saida": "19:30",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 2,
+                    "hora_saida": "19:20",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+                {
+                    "tipo_dia": 2,
+                    "hora_saida": "20:30",
+                    "observacao": "Horário realizado pela linha Jd. Colibri"
+                },
+            ],
+            "lat": null,
+            "lon": null,
+            "ordem": 0
+        }
+    ],
+    "shape": []
+})
